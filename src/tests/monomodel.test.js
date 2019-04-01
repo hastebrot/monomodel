0fc8aa70d6b13de6e16abb1f7fadb9445c2e8bf9
Benjamin Gudehus <hastebrot@gmail.com>
2019-04-02 00:44:12 +0200

    (feature) Add buildModel() to generate model from schema.

--- src/tests/monomodel.test.js
+++ src/tests/monomodel.test.js
@@ -1,33 +1,122 @@
+import { concat, get, set, pick } from "lodash"
 import { schemaWalk } from "@cloudflare/json-schema-walker"
-import { taskSchema, taskModel } from "./fixture"
-import { pretty } from "../utils"
-import { concat, get, set } from "lodash"
-import { Object } from "es6-shim"
+import { object, string, fieldset, field } from "./fixture"
 
 describe("monomodel", () => {
-  test("schema", () => {
-    console.log("schema", pretty(taskSchema))
-    console.log("model", pretty(taskModel))
-    console.log("inspected model", pretty(inspectModel(taskSchema)))
+  test("schema 1", () => {
+    // given:
+    const schema = object({
+      title: "order",
+    })
+    const model = fieldset("object", {
+      pointer: "#/",
+      title: "order",
+    })
+    // expect:
+    expect(buildModel(schema)).toEqual(model)
+  })
+
+  test("schema 2", () => {
+    // given:
+    const schema = object({
+      title: "order",
+      properties: {
+        orderNumber: string(),
+        orderDate: string(),
+      },
+    })
+    const model = fieldset("object", {
+      pointer: "#/",
+      title: "order",
+      children: [
+        field("string", {
+          pointer: "#/properties/orderNumber",
+        }),
+        field("string", {
+          pointer: "#/properties/orderDate",
+        }),
+      ],
+    })
+    // expect:
+    expect(buildModel(schema)).toEqual(model)
+  })
+
+  test("schema 3", () => {
+    // given:
+    const schema = object({
+      title: "order",
+      properties: {
+        orderNumber: string(),
+        orderDate: string(),
+        customer: object({
+          properties: {
+            customerNumber: string(),
+            firstName: string(),
+            lastName: string(),
+          },
+        }),
+      },
+    })
+    const model = fieldset("object", {
+      pointer: "#/",
+      title: "order",
+      children: [
+        field("string", {
+          pointer: "#/properties/orderNumber",
+        }),
+        field("string", {
+          pointer: "#/properties/orderDate",
+        }),
+        fieldset("object", {
+          pointer: "#/properties/customer",
+          children: [
+            field("string", {
+              pointer: "#/properties/customer/properties/customerNumber",
+            }),
+            field("string", {
+              pointer: "#/properties/customer/properties/firstName",
+            }),
+            field("string", {
+              pointer: "#/properties/customer/properties/lastName",
+            }),
+          ],
+        }),
+      ],
+    })
+    // expect:
+    expect(buildModel(schema)).toEqual(model)
   })
 })
 
-export const inspectModel = schema => {
-  const model = {}
+export const buildModel = schema => {
+  const patches = []
   schemaWalk(schema, (schemaObject, path, parentSchemaObject, parentPath) => {
-    const fullPath = toPath(concat(parentPath, path))
-    model[fullPath] = {
-      type: schemaObject.type,
-      keys: Object.keys(schemaObject),
-      pointer: toPointer(concat(parentPath, path)),
+    const schemaPath = toPath(concat(parentPath, path))
+    const pointer = toPointer(concat(parentPath, path))
+    const type = schemaObject.type
+    if (type === "object" || type === "array") {
+      const payload = pick(schemaObject, ["title"])
+      const node = fieldset(type, { pointer, ...payload })
+      const _path = schemaPath ? "root." + schemaPath : "root"
+      patches.push({ path: _path, node })
+    } else {
+      const payload = pick(schemaObject, ["title"])
+      const node = field(type, { pointer, ...payload })
+      const _path = "root." + schemaPath.replace("properties.", "children.")
+      patches.push({ path: _path, node })
     }
   })
-  return model
+  const model = {}
+  patches.forEach(patch => {
+    set(model, patch.path || "root", patch.node)
+  })
+  return model.root
 }
 
 export const toPath = pathSegments => {
   return pathSegments.join(".")
 }
+
 export const toPointer = pathSegments => {
   return `#/${pathSegments.join("/")}`
 }
