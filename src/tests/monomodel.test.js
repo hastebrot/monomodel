a1e8422ecd58d3ba2ae4da61e4e498388de6b0de
Benjamin Gudehus <hastebrot@gmail.com>
2019-04-01 23:42:20 +0200

    (feature) Add inspectModel().

--- src/tests/monomodel.test.js
+++ src/tests/monomodel.test.js
@@ -1,8 +1,33 @@
-import { taskSchema, fetchSchemaPaths } from "./fixture"
+import { schemaWalk } from "@cloudflare/json-schema-walker"
+import { taskSchema, taskModel } from "./fixture"
+import { pretty } from "../utils"
+import { concat, get, set } from "lodash"
+import { Object } from "es6-shim"
 
 describe("monomodel", () => {
   test("schema", () => {
-    console.log("schema", taskSchema)
-    console.log("schema paths", fetchSchemaPaths(taskSchema))
+    console.log("schema", pretty(taskSchema))
+    console.log("model", pretty(taskModel))
+    console.log("inspected model", pretty(inspectModel(taskSchema)))
   })
 })
+
+export const inspectModel = schema => {
+  const model = {}
+  schemaWalk(schema, (schemaObject, path, parentSchemaObject, parentPath) => {
+    const fullPath = toPath(concat(parentPath, path))
+    model[fullPath] = {
+      type: schemaObject.type,
+      keys: Object.keys(schemaObject),
+      pointer: toPointer(concat(parentPath, path)),
+    }
+  })
+  return model
+}
+
+export const toPath = pathSegments => {
+  return pathSegments.join(".")
+}
+export const toPointer = pathSegments => {
+  return `#/${pathSegments.join("/")}`
+}
