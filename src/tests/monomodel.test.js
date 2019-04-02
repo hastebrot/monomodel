bee2e991686f5a19607ed4c489e914dda94975e2
Benjamin Gudehus <hastebrot@gmail.com>
2019-04-02 09:53:26 +0200

    (feature) Create path registry for indices.

--- src/tests/monomodel.test.js
+++ src/tests/monomodel.test.js
@@ -1,4 +1,5 @@
 import { concat, get, set, pick } from "lodash"
+import produce from "immer"
 import { schemaWalk } from "@cloudflare/json-schema-walker"
 import { object, string, fieldset, field } from "./fixture"
 
@@ -89,26 +90,47 @@ describe("monomodel", () => {
 })
 
 export const buildModel = schema => {
-  const patches = []
+  const registry = {}
+  const model = {}
   schemaWalk(schema, (schemaObject, path, parentSchemaObject, parentPath) => {
-    const schemaPath = toPath(concat(parentPath, path))
     const pointer = toPointer(concat(parentPath, path))
+    const parentPointer = toPointer(parentPath)
     const type = schemaObject.type
+
+    if (pointer === parentPointer) {
+      registry[pointer] = {
+        parentPointer: null,
+        path: null,
+        index: 0,
+      }
+    } else {
+      registry[pointer] = {
+        parentPointer,
+        path,
+        index: registry[parentPointer].index,
+      }
+      registry[parentPointer].index += 1
+    }
+
+    const result = []
+    let current = registry[pointer]
+    result.unshift(current.path ? ["children", current.index] : [])
+    while (current.parentPointer) {
+      current = registry[current.parentPointer]
+      result.unshift(current.path ? ["children", current.index] : [])
+    }
+    const objectPath = toPath(concat(...result))
+    const rootObjectPath = objectPath ? "root." + objectPath : "root"
+
     if (type === "object" || type === "array") {
       const payload = pick(schemaObject, ["title"])
       const node = fieldset(type, { pointer, ...payload })
-      const _path = schemaPath ? "root." + schemaPath : "root"
-      patches.push({ path: _path, node })
+      set(model, rootObjectPath, node)
     } else {
       const payload = pick(schemaObject, ["title"])
       const node = field(type, { pointer, ...payload })
-      const _path = "root." + schemaPath.replace("properties.", "children.")
-      patches.push({ path: _path, node })
+      set(model, rootObjectPath, node)
     }
-  })
-  const model = {}
-  patches.forEach(patch => {
-    set(model, patch.path || "root", patch.node)
   })
   return model.root
 }
