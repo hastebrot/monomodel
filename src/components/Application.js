395690605392bf18b0f81f6da066486b8fbfefb6
Benjamin Gudehus <hastebrot@gmail.com>
2019-04-12 12:12:42 +0200

    (feature) Add form with drag-and-drop handlers.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,14 +1,108 @@
-import React, { Fragment } from "react"
-import { Box } from "fannypack"
+import React, { Fragment, useState } from "react"
+import { produce } from "immer"
+import { Box, Flex, Set, Group, Button, Label, Input, Pane } from "fannypack"
+import {
+  FormRoot,
+  ContextProvider,
+  defaultFormContext,
+  simpleFormModel,
+} from "./forms"
 import { PrettyCode } from "../utils"
 import { taskSchema, taskModel } from "../tests/fixture"
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
+
+export const Form = () => {
+  const [formModel, setFormModel] = useState(simpleFormModel())
+  const formContext = defaultFormContext(
+    produce(context => {
+      context.registry.FormPart = EditorFormPart
+      context.formModel = formModel
+      context.setFormModel = setFormModel
+    })
+  )
+  return (
+    <ContextProvider formContext={formContext}>
+      <FormRoot rootModel={formModel} />
+    </ContextProvider>
+  )
+}
+
+export const DropBox = ({ children, name, ...otherProps }) => {
+  return <Box {...otherProps}>{children}</Box>
+}
+
+export const DragBox = ({ children, name, ...otherProps }) => {
+  return (
+    <Box draggable {...otherProps}>
+      {children}
+    </Box>
+  )
+}
+
+const onDragStart = (event, path) => {
+  console.log("drag start:", path, event.currentTarget)
+  event.dataTransfer.setData("path", path)
+}
+
+const onDragOver = (event, path) => {
+  event.preventDefault()
+}
+
+const onDrop = (event, path) => {
+  event.preventDefault()
+  const data = event.dataTransfer.getData("path")
+  console.log("drop:", path, event.currentTarget, data)
+}
+
+const onMouseOver = (event, path) => {}
+
+export const EditorFormPart = ({ node, path, children }) => {
+  if (node.type === "fieldset") {
+    return (
+      <DropBox
+        name={path}
+        onDragOver={onDragOver}
+        onDrop={event => onDrop(event, path)}
+      >
+        <Pane padding="major-2" border>
+          <Box marginBottom="major-1" padding="major-1" background="orange">
+            <Flex row>
+              <Label flex="1">{node.pointer}</Label>
+              <Group>
+                <Button size="small">1-column</Button>
+                <Button size="small">2-column</Button>
+              </Group>
+            </Flex>
+          </Box>
+          <Set isVertical isFilled spacing="major-2">
+            {children}
+          </Set>
+        </Pane>
+      </DropBox>
+    )
+  }
+  if (node.type === "field") {
+    return (
+      <DragBox
+        name={path}
+        onMouseOver={event => onMouseOver(event, path)}
+        onDragStart={event => onDragStart(event, path)}
+      >
+        <Box marginBottom="major-1" padding="major-1" background="tomato" />
+        <Input defaultValue={node.pointer} />
+      </DragBox>
+    )
+  }
+  throw new Error("node.type handler not defined")
+}
 
 export default () => {
   return (
     <Fragment>
       <Box>
-        <PrettyCode value={taskSchema} />
-        <PrettyCode value={taskModel} />
+        <Form />
+        {/* <PrettyCode value={taskSchema} /> */}
+        {/* <PrettyCode value={taskModel} /> */}
       </Box>
     </Fragment>
   )
---
+++ src/components/forms.js
@@ -0,0 +1,155 @@
+import React, { Fragment, useContext } from "react"
+import { Box, Text, Code, ThemeProvider } from "fannypack"
+import { compact } from "lodash-es"
+
+//------------------------------------------------------------------------------
+// FORM COMPONENTS.
+//------------------------------------------------------------------------------
+
+export const FormRoot = ({ rootModel, rootView = null }) => {
+  const { registry, queryComponent } = useContext(FormContext)
+  return (
+    <Box
+      use={rootView || queryComponent(registry, "FormObject", rootModel)}
+      node={rootModel}
+      path={`root`}
+    />
+  )
+}
+
+export const FormObject = ({ node, path }) => {
+  const { registry, queryComponent } = useContext(FormContext)
+  return (
+    <Box
+      use={queryComponent(registry, "FormPart", node)}
+      node={node}
+      path={path}
+    >
+      {node.children && (
+        <Box
+          use={queryComponent(registry, "FormArray", node.children)}
+          items={node.children}
+          path={`${path}.children`}
+        />
+      )}
+    </Box>
+  )
+}
+
+export const FormArray = ({ items, path }) => {
+  const { registry, queryComponent } = useContext(FormContext)
+  return items.map((item, index) => (
+    <Fragment key={index}>
+      <Box
+        use={queryComponent(registry, "FormObject", item)}
+        node={item}
+        path={`${path}[${index}]`}
+      />
+    </Fragment>
+  ))
+}
+
+export const FormPart = ({ node, path, children }) => {
+  return (
+    <Box>
+      <Box
+        margin="major-1"
+        padding="major-1"
+        backgroundColor="danger"
+        color="white"
+      >
+        <Text>
+          Error: <Code>&lt;FormPart&gt;</Code> for type{" "}
+          <Code>{createFormPartType(node.type, node.datatype)}</Code> at path{" "}
+          <Code>{path}</Code> not found.
+        </Text>
+      </Box>
+      <Box>{children}</Box>
+    </Box>
+  )
+}
+
+export const createFormPartType = (...types) => {
+  return compact(types)
+    .join("-")
+    .toLowerCase()
+}
+
+//------------------------------------------------------------------------------
+// FORM CONTEXT.
+//------------------------------------------------------------------------------
+
+export const FormContext = React.createContext(null)
+
+export const ContextProvider = ({
+  localTheme = null,
+  formContext = null,
+  children,
+}) => {
+  return (
+    <ThemeProvider theme={localTheme}>
+      <FormContext.Provider value={formContext}>
+        <Fragment>{children}</Fragment>
+      </FormContext.Provider>
+    </ThemeProvider>
+  )
+}
+
+export const defaultFormContext = (modifyFormContext = context => context) => {
+  const context = {
+    registry: { FormObject, FormArray, FormPart },
+    queryComponent(registry, name, data) {
+      return registry[name]
+    },
+  }
+  return modifyFormContext(context)
+}
+
+export const defaultLocalTheme = (modifyLocalTheme = theme => theme) => {
+  return modifyLocalTheme(localTheme)
+}
+
+//------------------------------------------------------------------------------
+// FORM MODEL.
+//------------------------------------------------------------------------------
+
+export const buildFormModel = definitionFn => {
+  const helpers = { fieldset, field }
+  return definitionFn(helpers)
+}
+export const buildDefinition = (type, payload) => {
+  return { type, ...payload }
+}
+export const fieldset = (datatype, payload = {}) =>
+  buildDefinition("fieldset", { datatype, ...payload })
+export const field = (datatype, payload = {}) =>
+  buildDefinition("field", { datatype, ...payload })
+
+export const simpleFormModel = () => {
+  return fieldset("object", {
+    pointer: "#/",
+    title: "order",
+    children: [
+      field("string", {
+        pointer: "#/properties/orderNumber",
+      }),
+      field("string", {
+        pointer: "#/properties/orderDate",
+      }),
+      fieldset("object", {
+        pointer: "#/properties/customer",
+        children: [
+          field("string", {
+            pointer: "#/properties/customer/properties/customerNumber",
+          }),
+          field("string", {
+            pointer: "#/properties/customer/properties/firstName",
+          }),
+          field("string", {
+            pointer: "#/properties/customer/properties/lastName",
+          }),
+        ],
+      }),
+    ],
+  })
+}
